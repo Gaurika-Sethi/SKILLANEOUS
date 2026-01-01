8f6c05ba99fcb4b915b65a648bcbb9ae72ae55c3
Gaurikasethi <gaurikasethi88@gmail.com>
2026-01-01 14:46:49 +0530

    add express and CORS

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,4 +7,18 @@ dotenv.config({
     path: "./.env",
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error in starting the server", error);
+        process.exit(1);
+    });
+
+    app.listen(process.env.PORT||8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.log("Error in connecting to the database", error);
+    process.exit(1);
+});
