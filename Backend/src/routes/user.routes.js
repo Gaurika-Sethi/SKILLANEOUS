6a2e5559928080cdd7c707a471f96f93ca3628d9
Gaurikasethi <gaurikasethi88@gmail.com>
2026-01-08 23:30:28 +0530

    add change password, get current user, and update user profile functionalities

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -3,6 +3,10 @@ import { registerUser } from "../controllers/user.controller.js";
 import { loginUser } from "../controllers/user.controller.js";
 import{ logoutUser } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { refreshAccessToken } from "../controllers/user.controller.js";
+import { changePassword } from "../controllers/user.controller.js";
+import { getCurrentUser } from "../controllers/user.controller.js";
+import { updateUserProfile } from "../controllers/user.controller.js";
 
 const router = Router();
 
@@ -10,6 +14,8 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser); 
 router.route("/refresh-token").post(refreshAccessToken);
-
+router.route("/change-password").post(verifyJWT, changePassword);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/update-profile").post(verifyJWT, updateUserProfile);
 
 export default router;
